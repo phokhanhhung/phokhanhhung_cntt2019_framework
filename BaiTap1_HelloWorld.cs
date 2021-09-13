1c95103457031efb8a0e87d1641f8531e009e1de
phokhanhhung <phokhanhhung2409@gmail.com>
2021-09-13 11:32:43 +0700

    add BaiTap1

---
+++ BaiTap1_HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿// using System;
+
+// namespace Học_C_
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
